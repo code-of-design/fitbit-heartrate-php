d29a8574dcaa215a08740b86f3af36ae0ce0ddc9
Aiba Akihito <hash.codeofdesign@gmail.com>
2017-10-05 13:17:15 +0900

    心拍数取得のAjax通信

--- callback.php
+++ callback.php
@@ -1,8 +1,8 @@
 <?php
-  // 環境設定
-  require_once("env.php");
+  require_once("env.php"); // 環境設定
+  // require_once("heartrate.php"); // 心拍数
 
-  // アクセストークンとユーザIDを取得する
+  // アクセストークンを取得する
   // POSTヘッダを生成する
   $header = [
   	'Authorization: Basic ' . base64_encode(CLIENT_ID.':'.CLIENT_SECRET),
@@ -24,6 +24,7 @@
   		'ignore_errors' => true
   	)
   );
+  // レスポンス
   $context = stream_context_create($options);
   $response = file_get_contents(TOKEN_URL, false, $context);
   $token = json_decode($response, true); // アクセストークン
@@ -33,47 +34,31 @@
   	exit;
   }
   $access_token = $token['access_token']; // アクセストークン
-  $user_id = $token['user_id']; // ユーザID
-
-  // 心拍数を取得する
-  // $api_url = 'https://api.fitbit.com/1/user/' . $user_id . '/activities/heart/date/today/1d.json'; // Get Heart Rate Time Series
-  $api_url = 'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1sec.json'; // Get Heart Rate Intraday Time Series
-  // GETヘッダを生成する
-  $header = 'Authorization: Bearer ' . $access_token;
-  // $params = array('access_token' => $access_token); // アクセストークン
-  $options = array(
-  	'http' => array(
-  		'method' => 'GET',
-  		'header' => $header,
-  		'ignore_errors' => true
-  	)
-  );
-  $context = stream_context_create($options); // HTTPリクエストを$api_urlに対して送信する
-  $response = file_get_contents($api_url, false, $context); // レスポンス
-  // GETのレスポンスを取得する
-  $heartrate_json = $response; // 心拍数JSON
-  // echo $heartrate_json; // 表示する
-  // 心拍数JSONを心拍数配列にデコードする
-  $heatrate = json_decode($heartrate_json, true);
-  $heatrate_len = count($heatrate["activities-heart-intraday"]["dataset"]); // 1日の心拍数のログサイズ
+  // $user_id = $token['user_id']; // ユーザID
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Fitbit HeartRate PHP7.0</title>
-    <style>
-      p{
-        font-size: 48px;
-        margin: 20px;
-      }
-    </style>
   </head>
   <body>
-    <p>最新の心拍数</p>
+    <p>Fitbit API by PHP7.0 [ <a href="https://github.com/code-of-design/fitbit-heartrate-php">https://github.com/code-of-design/fitbit-heartrate-php</a> ]</p>
+    <!-- 心拍数 -->
     <?php
-      echo "<p>Time: ".$heatrate["activities-heart-intraday"]["dataset"][$heatrate_len-1]["time"]."</p>";
-      echo "<p>HeartRate: ".$heatrate["activities-heart-intraday"]["dataset"][$heatrate_len-1]["value"]."</p>";
+      for ($i=1; $i <= 15; $i++) {
+        echo "<span>Time: " . $heatrate["activities-heart-intraday"]["dataset"][$heatrate_len-$i]["time"] . "</span>";
+        echo "<span> HeartRate: ". $heatrate["activities-heart-intraday"]["dataset"][$heatrate_len-$i]["value"] ."</span><br>";
+      }
     ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+      function getHeartrate(){
+        $.post("heartrate.php", {"token": "<?php echo $access_token; ?>"},function(data){
+          console.log(data);
+        });
+      }
+      setInterval("getHeartrate()", 1000);
+    </script>
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
   require_once("env.php"); // 環境設定
 
-	// Fitbitの認証ページにリダイレクトする
+	// Fitbit APIの認証
 	$params = array(
 		'client_id' => CLIENT_ID,
 		'redirect_uri' => CALLBACK_URL,
 		'scope' => 'heartrate',
 		'response_type' => 'code',
 	);
-	header("Location: " . AUTH_URL . '?' . http_build_query($params)); // GET送信
+  //
+	header("Location: " . AUTH_URL . '?' . http_build_query($params));
+  exit();
 ?>
